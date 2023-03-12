973a0a3933551ffa3dfe96481f9c8a0790680ec1
uvcourses <uvcourses@gmail.com>
2023-03-12 01:02:05 -0600

    initial commit

---
+++ src/main/java/com/stream/streampractice/StreamPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.stream.streampractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stream/streampractice/StreamPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stream.streampractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
